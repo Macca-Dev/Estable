8ba295ebde1ec9f37f13d855edd481ccf8dbb029
mcinteer <ryan.mcinteer@gmail.com>
2015-02-12 21:06:57 +1300

    Include angular in the project
    Create the first controller and add the first data bindings to the page... EXCITING!

---
+++ EStable/Content/src/js/Wizard.js
@@ -0,0 +1,3 @@
+﻿(function() {
+    var wizard = angular.module("wizard", []);
+}());
---
+++ EStable/Content/src/js/controllers/WizardEmailControllerjs.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var WizardEmailController = function($scope) {
+
+        $scope.stableName = 'Stable Name';
+
+    };
+    
+    var wizard = angular.module("wizard");
+    wizard.controller("WizardEmailController", ["$scope", WizardEmailController]);
+}());
