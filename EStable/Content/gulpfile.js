106a47907b38f268a27e006cb6bd6521930c7326
Fay Pickering <fay.pickering@xero.com>
2015-02-12 22:07:54 +1300

    concat, copy minified ver, jshint js files into dist folder.Add minified css and js to index

--- EStable/Content/gulpfile.js
+++ EStable/Content/gulpfile.js
@@ -1,51 +1,62 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	watch = require('gulp-watch'),
-	autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    streamqueue = require('streamqueue'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    cache = require('gulp-cache');
+  sass = require('gulp-ruby-sass'),
+  watch = require('gulp-watch'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  streamqueue = require('streamqueue'), //not sure if we'll need this yet
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  cache = require('gulp-cache'); //we should use this but can add it later on
 
 
 gulp.task('css', function () {
-      return gulp.src('src/sass/**/*.scss')
-    .pipe(sass({ style: 'expanded' }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/sass/**/*.scss')
+  .pipe(sass({ style: 'expanded' }))
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(concat('all.css'))
+  .pipe(gulp.dest('dist/css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(minifycss())
+  .pipe(gulp.dest('dist/css'));
 });
 
+
+gulp.task('javascript', function(){
+  return gulp.src('src/js/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('dist/js'))
+	.pipe(uglify())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('dist/js'))
+})
 
 
 
 gulp.task('clean', function() {
   return gulp.src(['dist/css', 'dist/js'], {read: false})
-    .pipe(clean());
-
+  .pipe(clean());
+});
 
 // Watch
 gulp.task('watch', function () {
-    var cssWatcher = gulp.watch(['src/sass/**/*.scss'], ['styles']);
-    cssWatcher.on('change', function(event) {
-        console.log(event.type, event.path);
-    });
+  var cssWatcher = gulp.watch(['src/sass/**/*.scss'], ['css']);
+  cssWatcher.on('change', function(event) {
+      console.log(event.type, event.path);
+  });
 
-    // var jsWatcher = gulp.watch(['src/js/**/*.js'], ['scripts']);
-    // jsWatcher.on('change', function(event) {
-    //     console.log(event.type, event.path);
-    // });
- });
+  var jsWatcher = gulp.watch(['src/js/**/*.js'], ['javascript']);
+  jsWatcher.on('change', function(event) {
+      console.log(event.type, event.path);
+  });
 });
 
 //*****DEFAULT****//
 gulp.task('default', ['clean'], function() {
-    gulp.start('css');
+    gulp.start('css', 'javascript');
 });
