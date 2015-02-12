024e26c93e7c61c8149dc8d02c452c953b6c1306
mcinteer <ryan.mcinteer@gmail.com>
2015-02-12 22:10:41 +1300

    use angular routing instead of relying on the server to route us. I guess this means I'm building a single page application? :o that's what the smart kids talk about

--- EStable/Content/src/js/Wizard.js
+++ EStable/Content/src/js/Wizard.js
@@ -1,3 +1,15 @@
 ﻿(function() {
-    var wizard = angular.module("wizard", []);
+    var wizard = angular.module("wizard", ["ngRoute"]);
+
+    wizard.config(function($routeProvider) {
+
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/wizard/email.html",
+                controller: "WizardEmailController"
+            })
+            .otherwise({ redirectTo: "/" });
+    });
+
+
 }());
