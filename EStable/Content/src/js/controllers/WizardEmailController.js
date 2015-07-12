178abbb3c273fba06118ad3d151d74d2dfc5b14a
mcinteer <ryan.mcinteer@gmail.com>
2015-07-12 18:59:36 +1200

    dome FED things from a BED

---
+++ EStable/Content/src/js/controllers/WizardEmailController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var WizardEmailController = function($scope, wizardApi) {
+
+        $scope.emailLabel = 'Email: ';
+        $scope.submitEmail = function(email) {
+            wizardApi.postEmail(email)
+                .then(onPostEmailComplete, onError);
+        };
+
+        var onPostEmailComplete = function(data) {
+            console.log(data);
+        };
+
+        var onError = function(reason) {
+            console.log(reason);
+        };
+    };
+    
+    var wizard = angular.module("wizard");
+    wizard.controller("WizardEmailController", ["$scope", WizardEmailController]);
+}());
