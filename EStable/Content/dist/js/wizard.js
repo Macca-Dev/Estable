1e728de47e801f19f988cf64cd4e0a77a122938e
Fay Pickering <fay.pickering@xero.com>
2015-07-17 20:59:11 +1200

    bootstrap added to packages, css overrides

--- EStable/Content/dist/js/wizard.js
+++ EStable/Content/dist/js/wizard.js
@@ -16,36 +16,6 @@
     });
 }());
 
-(function() {
-    "use strict";
-    var wizard = angular.module("wizard"),
-    wizardApi = function($http) {
-        var postStable = function (data) {
-            //return $http.post("http://establewizardapi.azurewebsites.net/stable", data);
-                // .then(function(response) {
-                //     return response.data;
-                // });
-
-
-        return $http({
-            url: "http://establewizardapi.azurewebsites.net/stable",
-            method: "POST",
-            data: JSON.stringify(data),
-            withCredentials: true
-          });
-        };
-
-        return {
-            postStable: postStable
-        };
-    };
-    wizard.factory("wizardApi", wizardApi);
-    wizard.config(function($httpProvider) {
-      //Enable cross domain calls
-      $httpProvider.defaults.useXDomain = true;
-    });
-}());
-
 (function () {
     "use strict";
     var wizard = angular.module("wizard"),
@@ -114,4 +84,34 @@
         };
     };
     wizard.controller("wizardEmailController", ["$scope", "$rootScope", "$location", wizardEmailController]);
+}());
+
+(function() {
+    "use strict";
+    var wizard = angular.module("wizard"),
+    wizardApi = function($http) {
+        var postStable = function (data) {
+            //return $http.post("http://establewizardapi.azurewebsites.net/stable", data);
+                // .then(function(response) {
+                //     return response.data;
+                // });
+
+
+        return $http({
+            url: "http://establewizardapi.azurewebsites.net/stable",
+            method: "POST",
+            data: JSON.stringify(data),
+            withCredentials: true
+          });
+        };
+
+        return {
+            postStable: postStable
+        };
+    };
+    wizard.factory("wizardApi", wizardApi);
+    wizard.config(function($httpProvider) {
+      //Enable cross domain calls
+      $httpProvider.defaults.useXDomain = true;
+    });
 }());
