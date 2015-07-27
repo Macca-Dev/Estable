8721f1123ec32a0586ea6f79032f348663ca469a
mcinteer <ryan.mcinteer@gmail.com>
2015-07-27 21:56:11 +1200

    Do some FED things

--- EStable/Content/src/js/controllers/StableController.js
+++ EStable/Content/src/js/controllers/StableController.js
@@ -1,43 +1,28 @@
 (function(){
   "use strict";
-      var wizard = angular.module("wizard"),
-      onError = function(reason) {
-          console.log("reason", reason);
-      },
-      wizardStableController = function($scope, $rootScope, wizardApi){
-        var onPostStableComplete = function(data) {
-            console.log("data", data);
-            $rootScope.user.stableName = data.config.data.stableName;
-        },
+  var wizard = angular.module("wizard"),
+  wizardStableController = function($scope, $rootScope, wizardApi){
+    var onGetStableComplete = function(data){
+      $scope.stable = JSON.parse(data.data.Result);
+    },
+    onError = function(data){
+      console.log(data);
+    };
 
-        stable = {
-          stableName: "",
-          racingCode: "",
-          trainer: "",
-          legalEntity: "",
-          mobile: "",
-          phone: "",
-          fax: "",
-          address: ""
-        };
-
-       $scope.racingCode = stable.racingCode;
-       $scope.trainer = stable.trainer;
-       $scope.legalEntity = stable.legalEntity;
-       $scope.mobile = stable.mobile;
-       $scope.phone = stable.phone;
-       $scope.fax = stable.fax;
-       $scope.address = stable.address;
-       $scope.email = $rootScope.user.email;
-       $scope.stableName = stable.stableName;
-
-       $scope.postStable = function(stable) {
-         stable.stableEmail = $rootScope.user.email;
-         //var data = JSON.stringify(stable);
-         wizardApi.postStable(stable)
-          .then(onPostStableComplete, this.onError);
-        };
-
+    $scope.postStable = function(stable) {
+      var onPostStableComplete = function(data) {
+        $rootScope.user.stableName = data.config.data.stableName;
       };
-      wizard.controller("wizardStableController", ["$scope", "$rootScope", "wizardApi", wizardStableController]);
+
+      stable.stableEmail = $rootScope.user.email;
+
+      wizardApi.postStable(stable)
+      .then(onPostStableComplete, onError);
+    };
+
+    wizardApi.getStable($rootScope.user.email)
+    .then(onGetStableComplete, onError);
+
+  };
+  wizard.controller("wizardStableController", ["$scope", "$rootScope", "wizardApi", wizardStableController]);
 }());
--- EStable/Content/src/js/services/WizardApi.js
+++ EStable/Content/src/js/services/WizardApi.js
@@ -3,26 +3,30 @@
     var wizard = angular.module("wizard"),
     wizardApi = function($http) {
         var postStable = function (data) {
-            //return $http.post("http://establewizardapi.azurewebsites.net/stable", data);
-                // .then(function(response) {
-                //     return response.data;
-                // });
+            return $http({
+                //url: "http://establewizardapi.azurewebsites.net/stable",
+                url: "http://wizard.service/stable",
+                method: "POST",
+                data: data
+            });
+        };
 
-
-        return $http({
-            url: "http://establewizardapi.azurewebsites.net/stable",
-            method: "POST",
-            data: data
-          });
+        var getStable = function(email){
+            return $http({
+                //url: "http://establewizardapi.azurewebsites.net/stable/" + email,
+                url: "http://wizard.service/stable/" + email,
+                method: "GET",
+            });
         };
 
         return {
-            postStable: postStable
+            postStable: postStable,
+            getStable: getStable
         };
     };
     wizard.factory("wizardApi", wizardApi);
     wizard.config(function($httpProvider) {
       //Enable cross domain calls
       $httpProvider.defaults.useXDomain = true;
-    });
+  });
 }());
