ae6e254ae7af2f1976fb2a2fa6eb85dbc80f44b9
mcinteer <ryan.mcinteer@gmail.com>
2015-07-28 22:37:03 +1200

    dfg

--- EStable/Content/deploy.cmd
+++ EStable/Content/deploy.cmd
@@ -77,7 +77,7 @@ echo output something?
 
 :: 2. Run NPM
 npm install --prefix .\EStable\Content
-bower update --config.cwd=EStable\Content\
+bower install --config.cwd=EStable\Content\
 echo output something else
 GULP="%DEPLOYMENT_SOURCE%\EStable\Content\node_modules\gulp\bin\gulp.js"
 
