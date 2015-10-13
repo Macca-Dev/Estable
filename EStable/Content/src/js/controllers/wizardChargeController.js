03e0f36b821d62bbfdf4349c534db32015df42f3
Fay Pickering <fay.pickering@xero.com>
2015-10-13 20:13:32 +1300

    charget types grid, breif

--- EStable/Content/src/js/controllers/wizardChargeController.js
+++ EStable/Content/src/js/controllers/wizardChargeController.js
@@ -3,6 +3,22 @@
     var wizard = angular.module("wizard"),
     wizardChargeController = function($scope, $rootScope, wizardApi) {
 
+      $scope.stableCharges = [
+    {id: 1, description: 'awesome user1', unit: 2, rate: 4, inStable: true},
+    {id: 2, description: 'awesome user2', unit: 3, rate: 4, inStable: true},
+    {id: 3, description: 'awesome user3', unit: 4, rate: 7, inStable: false},
+    {id: 4, description: null, unit: 2, rate: 4, inStable: true},
+  ];
+
+      $scope.units =[
+        {value: 0, text: "Daily"},
+        {value: 1, text: "Weekly"},
+        {value: 2, text: "Fortnightly"},
+        {value: 3, text: "Monthly"},
+        {value: 4, text: "Quarterly"},
+        {value: 5, text: "Yearly"}
+      ];
+
     	var onGetChargeComplete = function(data){
             $scope.charge = JSON.parse(data.data);
             $scope.charge.TaxDate = new Date($scope.charge.TaxDate);
